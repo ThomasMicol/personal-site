97afb5d3a641f0191bc93123f58b4464cce2e0f7
Thomas Micol <thomasmicol@gmail.com>
2020-10-24 17:50:11 +1300

    properly set up the vue js app

--- src/main.js
+++ src/main.js
@@ -1,4 +1,41 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createStore } from 'vuex'
+import { createRouter, createWebHistory } from 'vue-router'
+import VueStickto from 'vue-stickto'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+import Projects from './components/Projects.vue'
+import Contact from './components/Contact.vue'
 
-createApp(App).mount('#app')
+const store = createStore({
+    devtools: true
+});
+
+const routes = [
+    {
+        path: '/home',
+        component: Home,
+    },
+    { path: '/about', component: About },
+    { path: '/projects', component: Projects },
+    { path: '/contact', component: Contact },
+
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(VueStickto);
+
+
+
+app.mount('#app');
+
+
