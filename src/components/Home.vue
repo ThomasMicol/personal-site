0dde7db33229338efb086e1157dff22e2cc1e833
Thomas Micol <thomasmicol@gmail.com>
2020-11-25 23:14:34 +1300

    scss changes and code style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="body-wrapper">
-    <div class="body-header">
-      Thomas Micol | Full Stack Developer
-    </div>
+    <div class="body-header">Thomas Micol | Full Stack Developer</div>
     <div class="home-content-wrapper">
-      <div class="home-content-division">this is some more content we will see how this displays</div>
-      <div class="home-content-division">This is some text for the home page component div 2</div>
+      <div class="home-content-division">
+        this is some more content we will see how this displays
+      </div>
+      <div class="home-content-division">
+        This is some text for the home page component div 2
+      </div>
     </div>
-    
   </div>
-
-  
 </template>
 
 <script>
@@ -19,11 +18,10 @@ export default {
 };
 </script>
 
-<style>
-.home-content-division{
+<style lang="scss">
+.home-content-division {
   width: 30em;
   padding: 1em;
   margin: 2em 0;
-
 }
 </style>
