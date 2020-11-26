a2bd7c1e52fa73d484057ff72d5d04cae717c591
Thomas Micol <thomasmicol@gmail.com>
2020-11-26 23:10:41 +1300

    changes to the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="footer-container">Footer</div>
+  <div class="footer-container">
+    <div class="footer-content-wrapper">
+      <div class="footer-content-column">
+        <div class="footer-content-container">
+          this is some content to the left
+        </div>
+      </div>
+      <div class="footer-content-column">
+        <div class="footer-content-container">
+          this is some contnet in the middle
+        </div>
+      </div>
+      <div class="footer-content-column last grid">
+        <div class="footer-content-container footer-call-to-action">
+          <button>Get in touch</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,5 +35,32 @@ export default {
   background-color: $secondary;
   display: grid;
   color: $dark;
+}
+
+.footer-content-column {
+  padding: 1em;
+
+  .last {
+    display: grid;
+  }
+}
+
+.footer-content-wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  .footer-call-to-action {
+    place-self: center;
+
+    button {
+      color: $light;
+      background-color: $highlight;
+      outline: 0;
+
+      height: 2.3em;
+      width: 10em;
+      border: 0px;
+    }
+  }
 }
 </style>
